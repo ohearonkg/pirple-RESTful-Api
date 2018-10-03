8312ca9c232c7e60d4e5f896750a5d0b3ca12ce2
K.G. O'Hearon <ohearonkg@gmail.com>
2018-10-03 13:52:21 -0300

    nice introduction to streams and how our API must handle the incoming request stream and append data to our string and only call our desired ending actions such as logging after we have successfully recieved the end event.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 // Dependancies
 var http = require("http");
 var url = require("url");
+var StringDecoder = require("string_decoder").StringDecoder;
 
 // Server should respond with a string
 var server = http.createServer(function(req, res) {
@@ -33,14 +34,40 @@ var server = http.createServer(function(req, res) {
   // Get the headers object
   var headers = req.headers;
 
-  // Send the response
-  res.end("Hello World!");
+  /**
+   * Get the payload if any
+   *
+   * NOTE: The request object is
+   * in the form of a stream. Hence
+   * we must append incoming data
+   * to our desired variable until
+   * such time as we have the entirity
+   * of the payload.
+   */
+  var decoder = new StringDecoder("utf-8");
+  var buffer = "";
+  req.on("data", function(data) {
+    buffer += decoder.write(data);
+  });
 
-  // Log the path requested
-  console.log("Path Requested: ", trimmedPath);
-  console.log("Method: ", method);
-  console.log("Query String: ", queryString);
-  console.log("Headers: ", headers, "\n");
+  /**
+   * When we are done with the request
+   * we perform our desired sending of
+   * the response and logging actions
+   */
+  req.on("end", function() {
+    buffer += decoder.end();
+
+    // Send the response
+    res.end("Hello World!");
+
+    // Log the path requested
+    console.log("Path Requested: ", trimmedPath);
+    console.log("Method: ", method);
+    console.log("Query String: ", queryString);
+    console.log("Headers: ", headers);
+    console.log("Payload: ", buffer);
+  });
 });
 
 // Start the server, and have it run on port 3000
