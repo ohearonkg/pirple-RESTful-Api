9116b2eddf9467e1ab7518f951d5f3fbf5839d5a
K.G. O'Hearon <ohearonkg@gmail.com>
2018-10-03 12:51:39 -0300

    basic server.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+/**
+ * Primary file for the API
+ */
+
+// Dependancies
+var http = require("http");
+
+// Server should respond with a string
+var server = http.createServer(function(req, res) {
+  res.end("Hello World\n");
+});
+
+// Start the server, and have it run on port 3000
+server.listen(3000, function() {
+  console.log("The server is listening on port 3000");
+});
