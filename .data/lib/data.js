7ba0760ad66c9d0c643842f6b2f912f9b3129e6f
K.G. O'Hearon <ohearonkg@gmail.com>
2018-10-04 10:52:14 -0300

    creating file to write data to file.

---
+++ .data/lib/data.js
@@ -0,0 +1,47 @@
+/**
+ * This file is used to read and write
+ * data from our file system simulating
+ * a database
+ *
+ * This library follows the error back pattern
+ * where a FALSE error will subsequently be a good
+ * thing and indicate we have no error
+ */
+
+var fs = require("fs");
+var path = require("path");
+
+var lib = {};
+
+// Base directory where our files will live
+lib.baseDir = path.join(__dirname, "/../.data/");
+
+/**
+ * This function is responsible for creating a file
+ * with the data passed to it
+ */
+lib.create = function(dir, fileName, data, callBack) {
+  fs.open(lib.baseDir + dir + "/" + fileName + ".json", "wx", function(
+    error,
+    fileDescriptor
+  ) {
+    if (!error && fileDescriptor) {
+      var stringData = JSON.stringify(data);
+      fs.writeFile(fileDescriptor, stringData, function(error) {
+        if (!error) {
+          fs.close(fileDescriptor, function(error) {
+            if (!error) {
+              callBack(false);
+            } else {
+              callback("Error Closing The File");
+            }
+          });
+        } else {
+          callback("Error Writing To The File");
+        }
+      });
+    } else {
+      callback("Could Not Create The File. It Most Likely Already Exists");
+    }
+  });
+};
